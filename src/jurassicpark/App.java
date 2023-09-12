7a3c942c3714836224607417275d69329649d1ce
Léa Gallier <100634711+LeaG76@users.noreply.github.com>
2023-09-12 13:46:08 +0200

    Change color background

--- src/jurassicpark/App.java
+++ src/jurassicpark/App.java
@@ -4,6 +4,7 @@ import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.stage.*;
 
 public class App extends Application {
@@ -14,6 +15,13 @@ public class App extends Application {
 
     public void start(Stage window) {
         window.setTitle("Jurassic Park Computer Screen");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 300, 300, Color.BLUE);
+
+        window.setScene(scene);
+
         window.show();
     }
 }

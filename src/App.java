70cbaf8861441c8be107faa9f2afe747bda6d5fd
Léa Gallier <100634711+LeaG76@users.noreply.github.com>
2023-09-12 12:16:08 +0200

    Update main class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage window) {
+        window.setTitle("Jurassic Park Computer Screen");
+        window.show();
     }
 }
